38f4af95c6c504f06c4e0a0fe79ac1a95b14f71d
Alex Bennison <abennison@doxa.partners>
2023-11-07 17:22:07 +0000

    Init setup

---
+++ DataSql/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataSql
+{
+    public class DataContext : DbContext
+    {
+        public string DbPath = "";
+
+        public DataContext()
+        {
+            //Get path to SQLite database file
+            Environment.SpecialFolder folder = Environment.SpecialFolder.LocalApplicationData;
+            string path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "SQLite/Performance.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,26 @@
+﻿using DataSql;
+using System;
+using System.Linq;
+using System.Reflection.Metadata;
+
+using DataContext db = new DataContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new ...");
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a ...");
+
+// Update
+Console.WriteLine("Updating the ... and adding a ...");
+
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the ...");
+
+db.SaveChanges();
