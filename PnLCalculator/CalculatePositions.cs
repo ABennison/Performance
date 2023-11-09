a6cec857d089b665270c5d3c1b3469e4acaab76b
James Thompson <jthompson@doxa.partners>
2023-11-09 15:38:35 +0000

    Restructure

--- PnLCalculator/CalculatePositions.cs
+++ PnLCalculator/CalculatePositions.cs
@@ -2,7 +2,7 @@
 namespace BusinessLogic
 {
     //Profit and Loss calculator
-    public class PnLCalculator
+    public class CalculatePositions
     {
         //Calculate price change on existing position
         //today's PnL = today's price * today's position - yesterday's price * yesterday's position
