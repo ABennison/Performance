9d37a0e67f066c568f567a97ab6c4815ae7335d0
James Thompson <jthompson@doxa.partners>
2023-11-10 15:48:32 +0000

    Instructions

--- PnLCalculator/Program.cs
+++ PnLCalculator/Program.cs
@@ -2,7 +2,7 @@
 using PnLCalculator;
 using System.Globalization;
 
-// Write results object to CSV (we haven't created this object in the starting project)
+// Get filename for results csv, and delete if existing 
 var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 var resultsPath = Path.Combine(documentsPath, "Performance.csv");
 if (File.Exists(resultsPath)) { File.Delete(resultsPath); }
@@ -14,16 +14,16 @@ using (var db = new DataContext())
     // YOUR CODE OR ENTRY TO YOUR CODE HERE 
 
 
+    // LINQ to SQL example: Read with WHERE clause 
+    // List<MarketPrices> instrumentOnePrices = db.MarketPrices.Where(x => x.InstrumentID = 1).ToList();
+
 };
 
-// LINQ to SQL example: Read with WHERE clause 
-// List<MarketPrices> instrumentOnePrices = db.MarketPrices.Where(x => x.InstrumentID = 1).ToList();
-
-// Write results object to CSV (we haven't created this object in the starting project)
+// Write results object to CSV (we haven't created this object in the template given to you)
 using (var writer = new StreamWriter(resultsPath)) 
 { 
     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture)) 
     {
-        //csv.WriteRecords(results);
+        //csv.WriteRecords(YOUR_RESULTS_LIST_OBJECT);
     }
 }
