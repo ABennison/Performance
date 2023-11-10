a3a698bb2c454679b5e8bb7c478903f6c9a13225
James Thompson <jthompson@doxa.partners>
2023-11-10 18:12:20 +0000

    add Currency data class

---
+++ PnLCalculator/DataClasses/Currency.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PnLCalculator.DataClasses
+{
+    [Table("Currency")]
+    public class Currency
+    {
+        [Key]
+        [Column("Code")]
+        [MaxLength(3)]
+        public string Code { get; set; }
+
+        [Column("Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
